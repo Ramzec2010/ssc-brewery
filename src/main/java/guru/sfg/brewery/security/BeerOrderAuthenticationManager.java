644f6124621e79e4097056ae32f32f89d88d88e2
roman.hushpit <roman.hushpit@globallogic.com>
2021-05-10 19:03:43 +0300

    Add complex configuration for customer

---
+++ src/main/java/guru/sfg/brewery/security/BeerOrderAuthenticationManager.java
@@ -0,0 +1,20 @@
+package guru.sfg.brewery.security;
+
+import guru.sfg.brewery.domain.security.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@Slf4j
+public class BeerOrderAuthenticationManager {
+
+    public boolean customerIdMatches(Authentication authentication, UUID customerId){
+        User authenticatedUser = (User) authentication.getPrincipal();
+        log.debug("Auth user customer id:" + authenticatedUser.getCustomer().getId());
+
+        return authenticatedUser.getCustomer().getId().equals(customerId);
+    }
+}
